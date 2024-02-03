f22df89030df197c2c903102afa74c3fd6f13b09
autodotua <autodotua@outlook.com>
2024-02-03 19:26:40 +0800

    实现了导出配置的功能

--- FrpGUI.Avalonia/Views/ControlBar.axaml.cs
+++ FrpGUI.Avalonia/Views/ControlBar.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
 using Avalonia.VisualTree;
 using FrpGUI.Avalonia.ViewModels;
 using FrpGUI.Avalonia.Views;
@@ -11,49 +12,72 @@ using FzLib.Avalonia.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace FrpGUI.Avalonia.Views;
 
 public partial class ControlBar : UserControl
 {
+    private MainViewModel viewModel;
+
     public ControlBar()
     {
         InitializeComponent();
     }
     private void AddRuleButton_Click(object sender, RoutedEventArgs e)
     {
-        var mainView = GetMainView();
-        Debug.Assert((mainView.DataContext as MainViewModel).CurrentPanel is ClientPanel);
-        var clientPanel = (mainView.DataContext as MainViewModel).CurrentPanel as ClientPanel;
+        Debug.Assert(viewModel.CurrentPanel is ClientPanel);
+        var clientPanel = viewModel.CurrentPanel as ClientPanel;
         clientPanel.AddRule();
     }
 
-    private MainView GetMainView()
+    private async void ExportButton_Click(object sender, RoutedEventArgs e)
     {
-        return this.GetVisualAncestors().OfType<MainView>().FirstOrDefault() ?? throw new System.Exception("找不到MainView");
-    }
-
-    private async void RestartButton_Click(object sender, RoutedEventArgs e)
-    {
-        var mainView = GetMainView();
         try
         {
-            await (mainView.DataContext as MainViewModel).CurrentFrpConfig.RestartAsync();
-        }
-        catch (Exception ex)
-        {
-            await this.GetWindow().ShowErrorDialogAsync("重启失败", ex);
-        }
-    }
-
-    private async void StartButton_Click(object sender, RoutedEventArgs e)
-    {
-        var mainView = GetMainView();
-        try
-        {
-            (mainView.DataContext as MainViewModel).CurrentFrpConfig.Start();
+            string config;
+            FilePickerFileType filter;
+            switch (AppConfig.Instance.FrpConfigType)
+            {
+                case "INI":
+                    config = viewModel.CurrentFrpConfig.ToIni();
+                    filter = new FilePickerFileType("TOML配置文件")
+                    {
+                        Patterns = ["*.toml"],
+                        MimeTypes = ["application/toml"]
+                    };
+                    break;
+                case "TOML":
+                    config = viewModel.CurrentFrpConfig.ToToml();
+                    filter = new FilePickerFileType("ini配置文件")
+                    {
+                        Patterns = ["*.ini"],
+                        MimeTypes = ["text/plain"]
+                    };
+                    break;
+                default:
+                    throw new Exception("未知FRP配置文件类型");
+            }
+            if (await (VisualRoot as Window).ShowYesNoDialogAsync("导出配置", "是否导出配置文件？", config) == true)
+            {
+                var file = await TopLevel.GetTopLevel(this).StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+                {
+                    FileTypeChoices = new[] { filter },
+                    SuggestedFileName = viewModel.CurrentFrpConfig.Name,
+                    DefaultExtension = filter.Patterns[0].Split('.')[1]
+                });
+                if (file != null)
+                {
+                    string path = file.TryGetLocalPath();
+                    if (path != null)
+                    {
+                        File.WriteAllText(path, config, new UTF8Encoding(false));
+                    }
+                }
+            }
         }
         catch (Exception ex)
         {
@@ -61,16 +85,15 @@ public partial class ControlBar : UserControl
         }
     }
 
-    private async void StopButton_Click(object sender, RoutedEventArgs e)
+    private async void RestartButton_Click(object sender, RoutedEventArgs e)
     {
-        var mainView = GetMainView();
         try
         {
-            await (mainView.DataContext as MainViewModel).CurrentFrpConfig.StopAsync();
+            await viewModel.CurrentFrpConfig.RestartAsync();
         }
         catch (Exception ex)
         {
-            await this.GetWindow().ShowErrorDialogAsync("停止失败", ex);
+            await this.GetWindow().ShowErrorDialogAsync("重启失败", ex);
         }
     }
 
@@ -79,4 +102,31 @@ public partial class ControlBar : UserControl
         await new SettingsWindow().ShowDialog(this.GetWindow());
     }
 
+    private async void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            viewModel.CurrentFrpConfig.Start();
+        }
+        catch (Exception ex)
+        {
+            await this.GetWindow().ShowErrorDialogAsync("启动失败", ex);
+        }
+    }
+    private async void StopButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await viewModel.CurrentFrpConfig.StopAsync();
+        }
+        catch (Exception ex)
+        {
+            await this.GetWindow().ShowErrorDialogAsync("停止失败", ex);
+        }
+    }
+    private void UserControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        viewModel = (this.GetVisualAncestors().OfType<MainView>().FirstOrDefault() ?? throw new System.Exception("找不到MainView"))
+            .DataContext as MainViewModel;
+    }
 }
